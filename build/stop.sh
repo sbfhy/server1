8be0c620ce74236065b800a08491ba9537165dfb
huangyong <huangyong@xd.com>
2021-10-18 18:38:09 +0800

    build/stop.sh关服修改

--- build/stop.sh
+++ build/stop.sh
@@ -4,12 +4,12 @@ oldIFS=$IFS
 IFS='
 '
 
-getServerPids="ps -u $LOGNAME | grep Server | grep -v grep | grep -v vim"   # $LOGNAME是用户名，这行筛选出所有Server进程
+getServerPids="ps -aux |grep $LOGNAME | grep Server | grep -v grep | grep -v vim |grep -v \"\-d\" "   # $LOGNAME是用户名，这行筛选出所有Server进程
 
 for var in $(echo $getServerPids | bash) 
 do 
-    pid=$(echo $var |awk '{ printf "%s ", $1}')
-    pname=$(echo $var |awk '{ printf "%s ", $4}')
+    pid=$(echo $var |awk '{ printf "%s ", $2 }')
+    pname=$(echo $var |awk '{ print substr($11,3) }')
 
     if kill -2 $pid 
     then 
@@ -20,3 +20,4 @@ do
 done
 
 IFS=$oldIFS
+
